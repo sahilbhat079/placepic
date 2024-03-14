3c235b09a42d4633b64ce8b74be8f024daca55a8
sahilbhat079 <sahilgani.sahilgani@gmail.com>
2024-03-14 13:03:19 +0530

    vercel config treated

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ import placesHandler from './api/places.js';
 import userPlacesHandler from './api/user-places.js';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
+
 const app = express();
 const PORT = 3000;
-
 
 // CORS configuration
 const corsOptions = {
   origin: '*', // Allow requests from any origin. Adjust as needed.
   methods: ['GET', 'PUT'], // Allow GET and PUT requests
 };
-// Serve images from the public/images directory
 
+// Serve images from the public/images directory
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const imagesPath = join(__dirname, './public/images');
 app.use('/images', express.static(imagesPath));
@@ -27,9 +27,10 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Define routes
-app.get("/",(res,req)=>{
+app.get("/", (req, res) => {
   res.send("hello world");
-})
+});
+
 app.use('/api/places', placesHandler);
 app.use('/api/user-places', userPlacesHandler);
 
